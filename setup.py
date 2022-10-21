76f1222a2eab66535a164559b5432c208cf623dc
Sergei Zobov <sergey@szobov.ru>
2022-10-21 09:20:27 +0200

    Add setup.py, requirements files and setup.cfg

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="anker",
+    version="0.0.1",
+    description="",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+    packages=find_packages(where="anker"),
+)
