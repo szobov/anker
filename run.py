3a42592fbb621ff77696845486d0d837fa143802
Sergei Zobov <sergey@szobov.ru>
2023-06-10 12:55:43 +0200

    run.py: fix linting issues

--- run.py
+++ run.py
@@ -3,8 +3,12 @@ import logging
 from anker.bot.bot import main
 from anker.card_generation.translation import initialize_translation_packages
 
-logging.basicConfig(format='[%(asctime)s] %(levelname)s in %(filename)s:%(lineno)d : %(message)s')
+logging.basicConfig(
+    format="[%(asctime)s] %(name)s %(levelname)s in "
+    "%(filename)s:%(lineno)d : %(message)s"
+)
 logging.getLogger().setLevel(logging.INFO)
+
 
 def initialize():
     initialize_translation_packages()
